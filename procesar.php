5dee267a8c0c4493e74f0ca7b6b210efe929c353
alfr3dh <115795606+alfr3dh@users.noreply.github.com>
2023-10-02 20:04:08 -0300

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -1,14 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $estado = $_POST["estado"];
+    $hora = $_POST["hora"];
+    $latitude = $_POST["latitude"];
+    $longitude = $_POST["longitude"];
 
-    // Aquí puedes realizar cualquier acción que desees con el valor recibido, como guardarlo en un archivo o realizar una acción específica.
+    // Aquí puedes realizar cualquier acción que desees con la hora y la geolocalización recibidas, como guardarlas en un archivo o realizar una acción específica.
     
-    // Por ejemplo, guardar el estado en un archivo de registro:
-    file_put_contents("registro.txt", $estado . "\n", FILE_APPEND);
+    // Por ejemplo, guardar la hora y la geolocalización en un archivo de registro:
+    $registro = "Hora: " . $hora . "\n";
+    $registro .= "Latitud: " . $latitude . "\n";
+    $registro .= "Longitud: " . $longitude . "\n";
+    file_put_contents("registro.txt", $registro, FILE_APPEND);
 
-    // Redireccionar de vuelta a la página de "sala102.html" o a donde desees.
-    header("Location: sala102.html");
+    // Redireccionar de vuelta a la página de "sala101.html" o a donde desees.
+    header("Location: sala101.html");
     exit;
 }
 ?>
